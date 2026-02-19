28e4ae43ac1a5f4466aec37eddb23128bc9d397d
Miranildo <miranildo@hotmail.com>
2026-02-19 07:05:25 -0300

    Update install-bot-nginx.sh

--- install-bot-nginx.sh
+++ install-bot-nginx.sh
@@ -780,7 +780,7 @@ cat > /etc/logrotate.d/botzap <<'LOGEOF'
     compress
     delaycompress
     notifempty
-    create 0640 botzap adm
+    create 0640 botzap www-data
     sharedscripts
     postrotate
         systemctl restart botzap.service 2>/dev/null || true
